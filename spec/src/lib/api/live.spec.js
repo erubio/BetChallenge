2868df876a7f012177f3627741387ea565af603a
Ezequiel Rubio <eze.rubio@gmail.com>
2015-04-28 00:20:03 +0200

    Update live requester

--- spec/src/lib/api/live.spec.js
+++ spec/src/lib/api/live.spec.js
@@ -5,17 +5,12 @@ var expect = require('chai').expect,
 	helpers = require('../../../helpers/test_helpers'),
 	live = require('../../../../src/lib/api/live');
 
-var	url = 'http://www.betvictor.com/live/en/live/list.json',
-	errorUrl = 'http://www.betvictor.com/live/en/live/listError.json';
 describe('Live json getter', function() {
 
 	before(function() {
 		nock('http://www.betvictor.com/')
 			.get('/live/en/live/list.json')
 			.reply(200, helpers.loadJsonFixture('/live.json'));
-		nock('http://www.betvictor.com/')
-			.get('/live/en/live/listError.json')
-			.reply(500);
 	});
 
 	after(function() {
@@ -23,7 +18,7 @@ describe('Live json getter', function() {
 	});
 		
 	it('Should get data from api', function(done) {
-		live.getData(url, function(err, response, body) {
+		live.getData(function(err, response, body) {
 			expect(err).to.equal(null);
 			expect(body).to.be.an('object');
 			expect(body.sports).to.be.an('array');
@@ -32,14 +27,5 @@ describe('Live json getter', function() {
 			expect(body.sports[0].events[0].outcomes.length).to.equal(3);
 			done();
 		});
-	});
-
-
-	it('Should throw an error if server fails', function(done) {
-		live.getData(errorUrl, function(err, response, body) {
-			expect(err).to.equal(null);
-			expect(response.statusCode).to.equal(500);
-			done();
-		});	
 	});
 });
--- src/lib/api/live.js
+++ src/lib/api/live.js
@@ -1,9 +1,12 @@
 'use strict';
 
-var request = require('request');
+var request = require('request'),
+	url = 'http://www.betvictor.com/live/en/live/list.json';
 
-module.exports.getData = function(url, callback) {
+module.exports.getData = function(callback) {
 	request({url: url, json: true}, function (error, response, body) {
 		callback(error, response, body);
 	});
 };
+
+
