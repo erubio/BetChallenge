50fc3f9906a5e9bc3770e7c904e8d781f4dfbe00
Ezequiel Rubio <eze.rubio@gmail.com>
2015-05-03 12:03:19 +0200

    Update an add error management to api requests

--- src/lib/api/live.js
+++ src/lib/api/live.js
@@ -1,12 +1,18 @@
 'use strict';
 
 var request = require('request'),
+	live = require('../../models/live'),
 	url = 'http://www.betvictor.com/live/en/live/list.json';
 
+
 module.exports.getData = function(callback) {
+	var liveObj;
 	request({url: url, json: true}, function (error, response, body) {
-		callback(error, response, body);
+		if (!error && response.statusCode === 200) {
+			liveObj = new live.LiveModel(body);;
+			callback(error, response, liveObj);
+		} else {
+			callback(true, response, body);
+		}
 	});
 };
-
-
